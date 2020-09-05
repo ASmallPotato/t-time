46a9c6313511dc6cb1f66d31b42da4bfa74fe816
Alex <alexhfokmh@gmail.com>
2020-09-05 14:35:10 +0800

    feat(refactor): timer

--- timer.py
+++ timer.py
@@ -5,84 +5,84 @@ from configparser import ConfigParser
 
 config = ConfigParser(delimiters=('='))
 config.read('fonts.ini')
-ini_digits = config._sections['digits']
 
-def parse_ini_font(multiline_block, digit):
-    return (
-        { digit: line }
-        for line
-        in multiline_block[1:].replace('_', ' ').split('\n')
-    )
+config_digits_section = config._sections['digits']
 
-def merge(dicts):
-    return { k: v for d in dicts for k, v in d.items() }
+font = {
+    char: lines_block.replace('_', ' ').split('\n')[1:]
+    for char, lines_block
+    in config_digits_section.items()
+}
+line_height = max(len(height) for height in font.values())
 
-lines_maps = zip(*[
-    parse_ini_font(digit, lines)
-    for lines, digit
-    in ini_digits.items()
-])
-fonts = [ merge(line_maps) for line_maps in lines_maps ]
+def wipe_screen(screen, colour):
+    screen._double_buffer = [
+        [(" ", colour, 0, colour)] * screen.width
+        for y in range(screen.height)
+    ]
 
-
-def wipe_screen(screen, bg):
-    for y in range(screen.height):
-        screen._double_buffer[y] = [(" ", bg, 0, bg)] * screen.width
-        # for x in range(screen.width):
-            # screen._double_buffer[y][x] = (" ", bg, 0, bg)
-
-def get_text_lines(text):
-    return ('  '.join(line[c] for c in text) for line in fonts)
+def get_text_lines(text, letter_spacing=2):
+    spacing = ' ' * letter_spacing
+    glyphs = (font.get(char) for char in text)
+    return (spacing.join(line) for line in zip(*glyphs))
 
 def timer(screen, frame_no, timer_config, ev):
+    if ev == ord('q'):
+        return 0
 
     if ev == ord('r') and timer_config.get('r_to_restart', False):
-        start_time = datetime.now()
-        hours_needed = 0.25
-        timer_config['target_time'] = start_time + timedelta(hours=hours_needed, seconds=0)
+        timer_config['_target_time'] = None
         wipe_screen(screen, Screen.COLOUR_BLACK)
 
+    if '_target_time' not in timer_config:
+        start_time = datetime.now()
+        timer_config['_target_time'] = start_time + timedelta(**timer_config['duration'])
 
-    def textWidth(offset):
+    def text_width(offset):
         return (screen.width + offset) // 2 + 1
 
     now = datetime.now()
-    over_time = False
+    is_over_time = False
 
-    _micro_sec_diff = (timer_config['target_time'] - now).total_seconds()
-    sec_diff = round(_micro_sec_diff)
+    micro_sec_diff = (timer_config['_target_time'] - now).total_seconds()
+    sec_diff = round(micro_sec_diff)
 
 
     if sec_diff < 0:
-        over_time = True
+        is_over_time = True
         sec_diff = abs(sec_diff)
 
     min_diff, seconds = divmod(sec_diff, 60)
     hours, minutes = divmod(min_diff, 60)
 
 
-    new_text = '+' if over_time else ''
+    new_text = '+' if is_over_time else ''
     new_text += '{}:'.format(hours) if hours else ''
     new_text += '{:02d}:{:02d}'.format(minutes, seconds)
-    # new_text += '{:02d}:{:02d}'.format(hours, minutes)
 
-    y = (screen.height // 2) - 4
+    y = (screen.height - line_height) // 2
     colour = Screen.COLOUR_GREEN
     bg = Screen.COLOUR_BLACK
 
-    if over_time:
-        is_odd_frame = _micro_sec_diff % 1 <= 0.5
-        bg = Screen.COLOUR_RED if is_odd_frame else Screen.COLOUR_WHITE
-        colour = Screen.COLOUR_RED if not is_odd_frame else Screen.COLOUR_WHITE
+    if is_over_time:
+        is_odd_frame = micro_sec_diff % 1 <= 0.5
+        if is_odd_frame:
+            bg = Screen.COLOUR_RED
+            colour = Screen.COLOUR_WHITE
+        else:
+            bg = Screen.COLOUR_WHITE
+            colour = Screen.COLOUR_RED
 
     wipe_screen(screen, bg)
     for i, line in enumerate(get_text_lines(new_text)):
-        screen.print_at(line,
-                        textWidth(-len(line)),
-                        y + i,
-                        colour=colour,
-                        bg=bg,
-                        transparent=True)
+        screen.print_at(
+            line,
+            text_width(-len(line)),
+            y + i,
+            colour=colour,
+            bg=bg,
+            transparent=True
+        )
 
     return 1
 
@@ -90,13 +90,13 @@ def timer(screen, frame_no, timer_config, ev):
 if __name__ == '__main__':
     from tui import TUI
 
-    target_time = datetime.now() + timedelta(hours=0, minutes=15, seconds=0)
+    target_time = datetime.now() + timedelta()
 
     timer_args = {
-        "target_time": target_time,
+        "duration": { "hours": 0, "minutes": 15, "seconds": 0 },
         "r_to_restart": True,
     }
 
-    tui = TUI(timer_args, timer)
+    tui = TUI({ **timer_args }, timer)
 
     tui.start()
